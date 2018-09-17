f6bb3af6d043531ebb2e3e4fc141d5b68f28c2d8
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-18 00:26:41 +0100

    Problem G Solved #5

--- src/mcpc2017/problem_a.kt
+++ src/mcpc2017/problem_a.kt
@@ -1,5 +1,14 @@
 package mcpc2017
 
 fun main(args: Array<String>) {
+    val test = readLine()!!.toInt()
 
+    for (i in 0 until test) {
+        val d = readLine()?.toInt()!!
+        val a = readLine()?.toInt()!!
+        val b = readLine()?.toInt()!!
+        val cubeOne = a * a * d
+        val cubeTwo = b * b * d
+        println("$cubeOne $cubeTwo ${cubeOne + cubeTwo}")
+    }
 }
---
+++ src/mcpc2017/problem_g.kt
@@ -0,0 +1,5 @@
+package mcpc2017
+
+fun main(args: Array<String>) {
+
+}
